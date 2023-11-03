3387fdd731b0b4bad6cfbdbd2d54c30dbc038407
valenpy22 <valentina.campos.o@usach.cl>
2023-11-03 19:08:00 -0300

    fix: config-data files

--- student-service/src/main/java/com/example/studentservice/controllers/StudentController.java
+++ student-service/src/main/java/com/example/studentservice/controllers/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/students")
 public class StudentController {
     @Autowired
